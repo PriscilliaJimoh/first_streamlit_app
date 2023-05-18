f1ad76028ad630d397684eb661ad4603715ca5f2
Pris <101253678+PriscilliaJimoh@users.noreply.github.com>
2023-05-18 10:59:00 +0100

    Update streamlit_app.py
    remving the stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ except URLError as e:
 
 add_my_fruit = streamlit.text_input('What fruit would you like information about?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
-streamlit.stop()
+# streamlit.stop()
 
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
